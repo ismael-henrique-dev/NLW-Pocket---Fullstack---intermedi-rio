3ac88eb6b07d2e1067a9cbb67db00d906d5715b8
Ismael <ismael.henrique.dev@gmail.com>
2024-09-20 21:25:35 -0300

    fix bug completionCount

--- src/http/routes/get-pending-goals.ts
+++ src/http/routes/get-pending-goals.ts
@@ -1,13 +1,13 @@
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 
-import { getWeekPenddingGoals } from '../../functions/get-week-pending-goals'
+import { getWeekPendingGoals } from '../../functions/get-week-pending-goals'
 
 export const getPenddingGoalsRoute: FastifyPluginAsyncZod = async app => {
   app.get('/pendding-goals', async () => {
-    const { penddingGoals } = await getWeekPenddingGoals()
+    const { pendingGoals } = await getWeekPendingGoals()
 
     return {
-      penddingGoals,
+      pendingGoals,
     }
   })
 }
